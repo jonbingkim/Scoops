29e9e375ff0bd1473741e2503a8090932baebcd1
jonbingkim <jonkimtw@gmail.com>
2025-01-23 13:29:31 -0800

    fixed sections and divs to better be separated from other sections

--- src/client_components/locations.tsx
+++ src/client_components/locations.tsx
@@ -6,23 +6,24 @@ export const Locations = () => {
   return (
     <section className="flex flex-col items-center justify-center gap-10">
 
-			<section className="flex items-center gap-8 justify-center p-5 ">
-				<div className="flex text-center justify-center">
+			<section className="flex items-center gap-8 justify-evenly p-5 ">
+				<div className="flex text-center">
+          <div>
 					<img className="h-90 w-96 rounded-lg shadow-lg transition-transform duration-300" src={yorkpic} alt="Scoops on York" />
-					<div className="absolute flex items-center justify-center">
+					<div className="absolute flex">
 						<a
 							href="https://www.yelp.com/biz/scoops-los-angeles-4"
 							target="_blank"
 							rel="noopener noreferrer"
 							className="p-3 border-2 border-white bg-black bg-opacity-70 text-white text-lg font-bold hover:bg-opacity-90 rounded-md transition-all duration-300 transform hover:scale-110"
 						>
-							{/* Scoops on York */}
+							Scoops on York
 						</a>
 					</div>
-					
+					</div>
 					
 			
-				<div className="max-w-1/2 justify-center p-6">
+				<div className="w-1/2 justify-center p-6">
 				<div className="">
 				<strong>Address: 5105 York Blvd,  Los Angeles,  CA  90042</strong>
 				</div>
@@ -34,26 +35,29 @@ export const Locations = () => {
 
 			<section className="flex items-center gap-8 justify-center p-5 ">
 				<div className="flex text-center justify-center">
-					<img className="h-90 w-96 rounded-lg shadow-lg transition-transform duration-300" src={yorkpic} alt="Scoops on York" />
-					<div className="absolute flex items-center justify-center">
+          <div>
+					<img className="h-90 w-96 rounded-lg shadow-lg transition-transform duration-300" src={chinatownpic} alt="Scoops on York" />
+					<div className="absolute flex">
 						<a
 							href="https://www.yelp.com/biz/scoops-los-angeles-4"
 							target="_blank"
 							rel="noopener noreferrer"
 							className="p-3 border-2 border-white bg-black bg-opacity-70 text-white text-lg font-bold hover:bg-opacity-90 rounded-md transition-all duration-300 transform hover:scale-110"
+              
 						>
-							{/* Scoops on York */}
+							Scoops Chinatown
 						</a>
 					</div>
+          </div>
 					
 					
 			
-				<div className="max-w-1/2 justify-center p-6">
-				<div className="">
-				<strong>727 N Broadway #125, Los Angeles, CA 90012</strong>
+				<div className="w-1/2 justify-center p-6">
+				<div>
+				<strong>Address: 727 N Broadway #125, Los Angeles, CA 90012</strong>
 				</div>
 				</div>
-				</div>
+        </div>
 			</section>
 				</section>
   )
