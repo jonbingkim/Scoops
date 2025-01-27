b55bc441913145510335684124056170eb098e3c
jonbingkim <jonkimtw@gmail.com>
2025-01-26 21:42:48 -0800

    maybe change the text in buzzfeed video

--- src/client_components/buzzfeed.tsx
+++ src/client_components/buzzfeed.tsx
@@ -2,7 +2,7 @@ export const Buzzfeed = () => {
   return (
     <div className="grid items-center justify-center align-center text-center p-6">
       <div className="flex justify-center">
-        check out our feature on worth it here :)
+        replace with certain text!
       </div>
       <div className="flex justify-center w-full">
         <iframe className="mx-auto" width="853" height="480" src="https://www.youtube.com/embed/rNuIOpfA2HA" title="$1 Ice Cream Vs. $1,000 Ice Cream" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
