2c7964cf0ce17a5fa0bfa0544b2690589b02fbb4
jonbingkim <jonkimtw@gmail.com>
2025-01-28 16:53:40 -0800

    responsiveness finished for the iframe

--- src/client_components/buzzfeed.tsx
+++ src/client_components/buzzfeed.tsx
@@ -1,11 +1,11 @@
 export const Buzzfeed = () => {
   return (
     <div className="grid items-center justify-center align-center text-center p-6">
-      <div className="flex justify-center">
-        replace with certain text!
+      <div className="flex justify-center font-fredoka text-xl">
+        As Featured on Buzzfeed Worth it :)
       </div>
-      <div className="flex justify-center w-full">
-        <iframe className="mx-auto" width="853" height="480" src="https://www.youtube.com/embed/rNuIOpfA2HA" title="$1 Ice Cream Vs. $1,000 Ice Cream" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      <div className="grid sm:w-full">
+        <iframe className=" w-full h-auto sm:w-300 h-full"  width="700" height="600" src="https://www.youtube.com/embed/rNuIOpfA2HA" title="$1 Ice Cream Vs. $1,000 Ice Cream" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
       </div>
     </div>
     
