05f3db91c724365bee146fa53fcdb3c39d7285c5
jonbingkim <jonkimtw@gmail.com>
2025-02-03 15:58:49 -0800

    changes to vh and vw for the iframe

--- src/client_components/buzzfeed.tsx
+++ src/client_components/buzzfeed.tsx
@@ -1,12 +1,17 @@
 export const Buzzfeed = () => {
   return (
-    <div className="grid items-center justify-center align-center text-center p-6">
+    <div className="h-auto w-auto grid items-center justify-center align-center text-center p-6">
       <div className="flex justify-center font-fredoka text-xl">
         As Featured on Buzzfeed Worth it :)
       </div>
-      <div className="grid sm:w-full">
-        <iframe className=" w-full h-auto sm:w-300 h-full"  width="700" height="600" src="https://www.youtube.com/embed/rNuIOpfA2HA" title="$1 Ice Cream Vs. $1,000 Ice Cream" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-      </div>
+      <div className=" overflow-hidden h-full w-full grid md:w-[40vw] h-[45vh]"> {/* Set a fixed height for the parent container */}
+  <iframe
+    className="w-full h-full" // Use full width and height of the parent
+    src="https://www.youtube.com/embed/rNuIOpfA2HA"
+    title="$1 Ice Cream Vs. $1,000 Ice Cream"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  ></iframe>
+</div>
     </div>
     
     
