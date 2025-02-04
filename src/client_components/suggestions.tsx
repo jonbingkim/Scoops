38a9938398604bad80e81ba985425d6a479bd2a0
jonbingkim <jonkimtw@gmail.com>
2025-02-03 21:00:15 -0800

    added purgomalum, a profanity filter

--- src/client_components/suggestions.tsx
+++ src/client_components/suggestions.tsx
@@ -1,9 +1,30 @@
+import { ReactEventHandler, useState } from "react"
+import axios from 'axios'
 
-
+const mappers = {
+  
+}
 export const Suggestion = () => {
+  const [suggestion, setSuggestion] = useState("")
+
+  const handlesubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const  res = await axios.get(`https://www.purgomalum.com/service/containsprofanity?text=${suggestion}`)
+    console.log(res.data)
+    if (res.data === false) {
+      console.log('hello!')
+    } else {
+      console.log('what the heck')
+    }
+    
+  }
   return (
     <div>
-      hello!
+      <form onSubmit={handlesubmit}>
+        <input type="text" placeholder="Suggest here!" value={suggestion} onChange={(e)=> setSuggestion(e.target.value)}></input>
+        <button type='submit'>submit</button>
+      </form>
+      
     </div>
   )
 }
