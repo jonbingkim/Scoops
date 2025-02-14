bd1fa9976a2c53f1026b82806f05bd6fe016cc76
jonbingkim <jonkimtw@gmail.com>
2025-02-13 18:59:43 -0800

    proper useeffect with fetching data, suggestion board looks better. just design left i believe.. the hard part..

--- src/client_components/suggestions.tsx
+++ src/client_components/suggestions.tsx
@@ -19,19 +19,22 @@ export const Suggestion = () => {
   const [suggestion, setSuggestion] = useState("");
   const [profane, setProfane] = useState(false);
   const [flavors, setFlavors] = useState([])
-  useEffect(() => {
-    const flavorData = async () => {
-      try {
-        const res = await axios.get('http://localhost:3000/');
-        console.log(res)
-        const flavors = res.data.flavors
-        setFlavors(flavors)
-      } catch (err) {
-        console.log('error found')
-      }
+  const [message, setMessage] = useState('')
+
+  const flavorData = async () => {
+    try {
+      const res = await axios.get('http://localhost:3000/');
+      console.log(res)
+      const flavorsData = res.data.flavors
+      setFlavors(flavorsData)
+    } catch (err) {
+      console.log('error found')
     }
+  }
+
+  useEffect(() => {
     flavorData()
-  }, [flavors.length]);
+  }, []);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -40,7 +43,9 @@ export const Suggestion = () => {
       const char = suggestion[i];
       newStr += map[char] || char;
     }
+    setMessage('')
     console.log(newStr);
+   
 
     try {
       // trying to see fi i need to use this either if im using filtering with perspective and openAI 
@@ -52,6 +57,21 @@ export const Suggestion = () => {
        await axios.post('http://localhost:3000/suggestion' ,{
           'text' : newStr
         })
+        .then(res => {
+          const jsonData =res.data
+          const message = jsonData.message
+          const error = jsonData.error
+          if (message) {
+            setMessage(message)
+          } else {
+            setMessage(error)
+          }
+        })
+        flavorData()
+        .catch(error => {
+          console.log(error)
+        })
+        
        console.log('heey from post')
       } else {
     
@@ -60,10 +80,12 @@ export const Suggestion = () => {
     } catch (error) {
       console.error("Error checking profanity:", error);
     }
+    setSuggestion('')
   };
 
   return (
     <div className="">
+      <Header/>
       {profane ? (
         <div className="items-center text-center">
           <div className="flex justify-center items-center bg-white bg-opacity-90">
@@ -78,34 +100,38 @@ export const Suggestion = () => {
        
         </div>
       ) : (
-        <div className="flex justify-center items-center min-h-screen">
-            {flavors.length > 0 ? (
-              flavors.map((flavor, idx) => (
-                <div key={idx}>{flavor}</div>
-              ))
-            ) : (
-              <div>No flavors available</div>
-            )}
-
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-4 items-center"
+        <div>
+        <div className="inset-0 flex flex-col items-center justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="relative z-10 w-auto max-w-md p-6 bg-white shadow-lg rounded-lg text-center"
+        >
+          <input
+            type="text"
+            placeholder="Flavor Suggestion Here!"
+            value={suggestion}
+            onChange={(e) => setSuggestion(e.target.value)}
+            className="w-auto px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            type="submit"
+            className="w-full mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            <input
-              type="text"
-              placeholder="Flavor Suggestion Here!"
-              value={suggestion}
-              onChange={(e) => setSuggestion(e.target.value)}
-              className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              Submit
-            </button>
-          </form>
+            Submit
+          </button>
+        </form>
+       <span> {message}</span>
+       </div>
+     
+        <div className="relative min-h-full flex items-center justify-center p-8">
+        <div className="relative inset-0 flex flex-wrap gap-1 p-8 text-base text-gray-700 leading-none">
+          {flavors.map((flavor, idx) => (
+            <span key={idx} className="inline leading-none">{flavor} •</span>
+          ))}
         </div>
+      </div>
+      </div>
+      
       )}
     </div>
   );
